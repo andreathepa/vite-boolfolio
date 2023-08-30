327d09230ed82109801d7a1871dac4922d040d29
andreathepa <andreadepa32@gmail.com>
2023-08-30 18:09:11 +0200

    MILESTONE 4 exercise done

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,28 +6,34 @@ export default {
       return{
         baseUrl:'http://localhost:8000',
         projects: [],
-        maxNumCharacters: 200,
-        currentPage:1,
-        lastPage:null,
         }
     },
     created(){
-      this.getProjects(1);
+      this.getProjects();
     },
     methods:{
-      getProjects(num_page){
+      getProjects(){
 
 
-        axios.get(`${this.baseUrl}/api/projects`, { params: {page: num_page}}).then(( response) => {
+        axios.get(`${this.baseUrl}/api/projects`).then(( response) => {
           if(response.data.success){
-            this.projects = response.data.results.data;
-            this.currentPage = response.data.results.current_page;
-            this.lastPage = response.data.results.last_page;
+            this.projects = response.data.results;
+
+            
+
+          }
+          else{
 
           }
 
-        });
+        })
 
+      },
+      truncateText(text){
+        if(text.length > 50){
+          return text.substr(0,50) + '...';
+        }
+        return text;
       }
     }
   }
@@ -46,31 +52,16 @@ export default {
     <div class="row">
       <div class="col-12 col-md-4" v-for="project in projects" :key="project.id">
         <div class="card my-3">
-          <div class="card-header">
+          <div class="card-header bg-primary text-light">
             {{ project.title }}
           </div>
-          <!-- <div class="card-image-top">
-            <img :src="${baseUrl}/storage/${project.image}" alt="img">
-          </div> -->
           <div class="card-body">
-            <div>
-              <span v-if="project.category">{{ project.category.name }}</span>
-              <span v-else>Categoria non assegnata</span>
-            </div>
-            <div v-if="project.technologies">
-              <span class="badge text-bg-primary me-3" v-for="technology in project.technologies" :key="technology.id">
-                {{ technology.name }}
-              </span>
-            </div>
-            <div>
-              {{ truncateText(project.content) }}
-            </div>
-          </div>
-          <div>
-            <a href="#" class="btn btn-sm btn-primary">Leggi il progetto</a>
-          </div>
+            {{ truncateText(project.content) }}
+          </div>  
+
         </div>
       </div>
+
     </div>
   </div>
 </template>
