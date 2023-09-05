483411ce61c925ba034c979b4d82e10b31a119fa
andreathepa <andreadepa32@gmail.com>
2023-09-05 12:01:49 +0200

    new

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,61 @@
+<script>
+import { store } from '../store';
+
+
+    export default {
+        name:"ProjectsCard",
+
+        props:{
+            project: Object
+        },
+
+        data() {
+            return {
+                maxNumChar:50,
+                store,
+                
+            }
+        },
+
+        methods: {
+            truncateText(text){
+                    if(text.length > this.maxNumChar){
+                        return text.substr(0,50) + '...'
+                    }
+                    return text;
+                }
+            
+        },
+
+
+
+
+        
+    }
+
+</script>
+
+
+<template lang="">
+     
+            <div class="card my-3">
+                <div class="card-header">
+                    {{project.title}}
+                </div>
+                <div class="card-body">
+                    {{truncateText(project.content)}}
+                </div>
+                <div class="card-footer">
+                    <!-- <a href="" class="btn btn-sm btn-primary"> Guarda il progetto</a> -->
+                    <router-link class="btn btn-sm btn-primary" :to="{name:'SingleProject', params:{slug:project.slug}}">Guarda il progetto</router-link>
+                </div>
+            </div>
+       
+       
+</template>
+
+
+
+<style lang="">
+    
+</style>
